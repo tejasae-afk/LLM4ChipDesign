bb45467678961019ec02c08de7ecd8979b52ef21
Tejas Attarde <tejasattardexx@gmail.com>
2026-03-01 05:06:45 -0500

    Add files via upload

---
+++ Rome_Submission/full_adder/full_adder.v
@@ -0,0 +1,25 @@
+module half_adder(input a, input b, output sum, output cout);
+  assign sum = a ^ b;
+  assign cout = a & b;
+endmodule
+module full_adder(input a, input b, input cin, output sum, output cout);
+  wire sum1, c1, c2;
+
+  half_adder ha1(.a(a), .b(b), .sum(sum1), .cout(c1));
+  half_adder ha2(.a(sum1), .b(cin), .sum(sum), .cout(c2));
+  assign cout = c1 | c2;
+endmodule
+module adder4(input [3:0] a, input [3:0] b, input cin, output [3:0] sum, output cout);
+  wire c1, c2, c3;
+
+  full_adder fa0(.a(a[0]), .b(b[0]), .cin(cin),   .sum(sum[0]), .cout(c1));
+  full_adder fa1(.a(a[1]), .b(b[1]), .cin(c1), .sum(sum[1]), .cout(c2));
+  full_adder fa2(.a(a[2]), .b(b[2]), .cin(c2), .sum(sum[2]), .cout(c3));
+  full_adder fa3(.a(a[3]), .b(b[3]), .cin(c3), .sum(sum[3]), .cout(cout));
+endmodule
+module ripple_carry_adder_8bit(input [7:0] a, input [7:0] b, input cin, output [7:0] sum, output cout);
+  wire c4;
+
+  adder4 adder4_0(.a(a[3:0]), .b(b[3:0]), .cin(cin), .sum(sum[3:0]), .cout(c4));
+  adder4 adder4_1(.a(a[7:4]), .b(b[7:4]), .cin(c4), .sum(sum[7:4]), .cout(cout));
+endmodule
